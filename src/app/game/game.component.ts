32ebc05742a8f447c7424b2a01d60e39468a98ad
rossmawdsley <ross.mawdsley@newday.co.uk>
2020-03-18 17:39:40 +0000

    image is displaying from card but it is huge

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { WelcomeService } from "../welcome/welcome.service";
-import { CardService } from '../card/card.service';
-import {ICard} from '../card/card'
-
+import { CardService } from "../card/card.service";
+import { ICard } from "../card/card";
 
 @Component({
   selector: "app-game",
@@ -12,8 +11,10 @@ import {ICard} from '../card/card'
 })
 export class GameComponent implements OnInit {
   playerName: string;
-  errorMessage: string
-  cards: ICard[] = []
+  errorMessage: string;
+  cards: ICard[] = [];
+  playerCards: ICard[];
+  opponentCards: ICard[];
 
   constructor(
     private route: ActivatedRoute,
@@ -25,33 +26,37 @@ export class GameComponent implements OnInit {
     this.cardService.getCards().subscribe({
       next: cards => {
         this.cards = cards;
-        console.log(this.distributeCards([...this.cards]))
+        this.distributeCards([...this.cards])
       },
       error: err => (this.errorMessage = err)
-    })
+    });
 
- 
     //this.playerName = this.route.snapshot.paramMap.get("name");
-   this.playerName ? null : this.playerName = this.welcomeService.getUserName()
-  
-  }
-  distributeCards(cards: ICard[]): ICard[][] {
-    let playerCards: ICard[]
-    let opponentCards: ICard[]
-    
-    cards = this.shuffle(cards)
-    
-
-
-    return [[this.cards.pop()],[this.cards.shift()]]
-      
+    this.playerName
+      ? null
+      : (this.playerName = this.welcomeService.getUserName());
   }
 
-   shuffle(cards: ICard[]): ICard[] {
+  distributeCards(cards: ICard[]): void {
+    let playerCards: ICard[];
+    let opponentCards: ICard[];
+
+    cards = this.shuffle(cards);
+    cards.pop();
+
+    playerCards = cards.slice(0, 8);
+    opponentCards = cards.slice(8);
+    console.log("players:", playerCards, "Opponents:", opponentCards);
+    
+    this.playerCards = playerCards
+    this.opponentCards = opponentCards
+  }
+
+  shuffle(cards: ICard[]): ICard[] {
     for (let i = cards.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [cards[i], cards[j]] = [cards[j], cards[i]];
+      const j = Math.floor(Math.random() * (i + 1));
+      [cards[i], cards[j]] = [cards[j], cards[i]];
     }
     return cards;
-}
+  }
 }
