b0cd397a2f7ac0d0d6c6b6d7bb4e31780ca8ba91
rossmawdsley <ross.mawdsley@newday.co.uk>
2020-03-19 14:45:38 +0000

    attributes display and cards look acceptable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,7 @@ import {FormsModule} from '@angular/forms'
 import { HttpClientModule } from "@angular/common/http";
 import {MatSliderModule}  from '@angular/material/slider';
 import {MatCardModule} from '@angular/material/card';
-import {MatGridListModule} from '@angular/material/grid-list';
-
+import {MatButtonModule} from '@angular/material/button';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -33,7 +32,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserAnimationsModule,
     MatSliderModule,
     MatCardModule,
-    MatGridListModule
+    MatButtonModule
    
     
   ],
